10cb56b8a6410ed86cd1f6604994158ba2fcb400
Thilan <thil4n@gmail.com>
2025-02-02 00:22:09 +0530

    gradle init

---
+++ app/src/main/java/org/example/HelloWorld.java
@@ -0,0 +1,33 @@
+package app.src.main.java.org.example;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+class HelloWorld {
+    private static void readAbi(){
+        File abi = new File("SimpleStorage.json");
+
+        try {
+            Scanner abiReader = new Scanner(abi);
+
+            for (int i = 0; i < 10; i++) {
+
+                String data = abiReader.nextLine();
+                System.out.println(data);
+                
+            }
+
+            abiReader.close();
+        } catch (FileNotFoundException  e) {
+            System.out.println("File not found!");
+        }
+
+
+    }
+    public static void main(String[] args){
+        readAbi();
+        System.out.println("hello world");
+    }
+}
