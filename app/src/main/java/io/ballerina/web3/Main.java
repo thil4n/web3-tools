8d895542500049ddd2085d76325a862752b7d14b
Thilan <thil4n@gmail.com>
2025-02-03 15:32:41 +0530

    update

--- app/src/main/java/io/ballerina/web3/Main.java
+++ app/src/main/java/io/ballerina/web3/Main.java
@@ -4,19 +4,11 @@ import io.ballerina.compiler.syntax.tree.*;
 import io.ballerina.tools.text.TextDocument;
 import io.ballerina.tools.text.TextDocuments;
 
-import static io.ballerina.compiler.syntax.tree.AbstractNodeFactory.*;
 import static io.ballerina.compiler.syntax.tree.NodeFactory.*;
 import static io.ballerina.compiler.syntax.tree.SyntaxKind.*;
 
 import java.util.ArrayList;
 import java.util.List;
-
-
-// SyntaxKind, MetadataNode, NodeList<Token>, Token, IdentifierToken, NodeList<Node>, FunctionSignatureNode, FunctionBodyNode
-// null,       null,         Token,                  IdentifierToken,                 FunctionSignatureNode, FunctionBodyNode
-
-
-
 
 public class Main {
     public static void main(String[] args) {
@@ -29,78 +21,55 @@ public class Main {
             createEmptyNodeList(), // No qualifiers
             generateRemoteFunctionSignature(), // Function parameters
             generateRemoteFunctionBody() // Function body
-    );
-
+        );
 
         NodeList<ModuleMemberDeclarationNode> members = createNodeList(functionNode);
         ModulePartNode modulePartNode = createModulePartNode(createEmptyNodeList(), members, createToken(EOF_TOKEN));
 
-        TextDocument textDocument = TextDocuments.from("");
-        SyntaxTree syntaxTree = SyntaxTree.from(textDocument);
-        syntaxTree.modifyWith(modulePartNode);
+        // Corrected: Convert ModulePartNode to a source text
+        String sourceCode = modulePartNode.toSourceCode();
 
-            System.out.println(syntaxTree.toSourceCode());
-            System.out.println("syntaxTree.toSourceCode()");
-        
+        System.out.println(sourceCode);
     }
 
-    /**
-     * Generates the client class remote function body.
-     *
-     * @param queryDefinition the object instance of a single query definition in a
-     *                        query document
-     * @param graphQLSchema   the object instance of the GraphQL schema (SDL)
-     * @param authConfig      the object instance representing authentication
-     *                        configuration information
-     * @return the node which represent the remote function body
-     */
     public static FunctionBodyNode generateRemoteFunctionBody() {
         List<StatementNode> assignmentNodes = new ArrayList<>();
-
         NodeList<StatementNode> statementList = createNodeList(assignmentNodes);
 
-        return createFunctionBodyBlockNode(createToken(OPEN_BRACE_TOKEN),
-                null, statementList, createToken(CLOSE_BRACE_TOKEN), null);
+        return createFunctionBodyBlockNode(
+            createToken(OPEN_BRACE_TOKEN),
+            null, statementList,
+            createToken(CLOSE_BRACE_TOKEN),
+            null
+        );
     }
 
-    /**
-     * Generates the client class remote function signature.
-     *
-     * @param queryDefinition the object instance of a single query definition in a
-     *                        query document
-     * @param graphQLSchema   the object instance of the GraphQL schema (SDL)
-     * @return the node which represent the remote function signature
-     */
     public static FunctionSignatureNode generateRemoteFunctionSignature() {
-        // Define a parameter: `int amount`
-        RequiredParameterNode param1 = NodeFactory.createRequiredParameterNode(
-            createEmptyNodeList(), // No annotations
-            NodeFactory.createBuiltinSimpleNameReferenceNode(null, createIdentifierToken("int")), // Type
-                createIdentifierToken("amount") // Parameter name
+        RequiredParameterNode param1 = createRequiredParameterNode(
+            createEmptyNodeList(),
+            createBuiltinSimpleNameReferenceNode(null, createIdentifierToken("int")),
+            createIdentifierToken("amount")
         );
 
-        // Define another parameter: `string name`
-        RequiredParameterNode param2 = NodeFactory.createRequiredParameterNode(
-            createEmptyNodeList(), // No annotations
-                NodeFactory.createBuiltinSimpleNameReferenceNode(null, createIdentifierToken("string")),
-                createIdentifierToken("name"));
+        RequiredParameterNode param2 = createRequiredParameterNode(
+            createEmptyNodeList(),
+            createBuiltinSimpleNameReferenceNode(null, createIdentifierToken("string")),
+            createIdentifierToken("name")
+        );
 
-        // Initialize a SeparatedNodeList with parameters
-        SeparatedNodeList<ParameterNode> parameterList = NodeFactory.createSeparatedNodeList(
-                param1, createToken(SyntaxKind.COMMA_TOKEN), param2);
+        SeparatedNodeList<ParameterNode> parameterList = createSeparatedNodeList(
+            param1, createToken(SyntaxKind.COMMA_TOKEN), param2
+        );
 
         BuiltinSimpleNameReferenceNode returnType = createBuiltinSimpleNameReferenceNode(null, createIdentifierToken("int"));
 
         ReturnTypeDescriptorNode returnTypeDescriptorNode = createReturnTypeDescriptorNode(
-                createToken(RETURNS_KEYWORD), createEmptyNodeList(), returnType);
+            createToken(RETURNS_KEYWORD), createEmptyNodeList(), returnType
+        );
 
         return createFunctionSignatureNode(
-                createToken(OPEN_PAREN_TOKEN), parameterList, createToken(CLOSE_PAREN_TOKEN), returnTypeDescriptorNode);
+            createToken(OPEN_PAREN_TOKEN), parameterList,
+            createToken(CLOSE_PAREN_TOKEN), returnTypeDescriptorNode
+        );
     }
-
-    public static String getRemoteFunctionSignatureReturnTypeName(String operationName) {
-        return operationName.substring(0, 1).toUpperCase() +
-                operationName.substring(1).concat("Response|graphql:ClientError");
-    }
-
 }
